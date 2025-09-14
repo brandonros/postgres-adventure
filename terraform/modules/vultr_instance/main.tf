92d460f9d7e97f620c75b104be4156f27ba6ddd9
Brandon Ros <brandonros1@gmail.com>
2025-09-14 15:46:38 -0400

    manifest

--- terraform/modules/vultr_instance/main.tf
+++ terraform/modules/vultr_instance/main.tf
@@ -49,6 +49,6 @@ runcmd:
   # install k9s
   - curl -L -O https://github.com/derailed/k9s/releases/download/v0.50.9/k9s_linux_amd64.deb && apt install -y ./k9s_linux_*.deb && rm k9s_linux_*.deb
   # install postgres
-  - curl -L -O https://raw.githubusercontent.com/brandonros/postgres-adventure/cde3577ed719ebeee54e2f52a73fbddada97bef3/manifests/postgresql.yaml && kubectl apply -f postgresql.yaml
+  - curl -L -O https://raw.githubusercontent.com/brandonros/postgres-adventure/c8f7a96ff5d5d27698f3f91af433fe55cd69cf24/manifests/postgresql.yaml && kubectl apply -f postgresql.yaml && rm postgresql.yaml
 EOF
 }
