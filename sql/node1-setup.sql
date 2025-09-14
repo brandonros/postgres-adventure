bd9be5b6c69b7fcd0cd30c8088cc4caba4541854
Brandon Ros <brandonros1@gmail.com>
2025-09-14 12:58:36 -0400

    sql scripts

---
+++ sql/node1-setup.sql
@@ -0,0 +1,73 @@
+-- database
+CREATE DATABASE my_db;
+
+-- user
+CREATE USER replicator WITH REPLICATION LOGIN;
+ALTER USER replicator PASSWORD 'test';
+GRANT CONNECT ON DATABASE my_db TO replicator;
+
+-- extension
+CREATE EXTENSION pglogical;
+
+-- Grant table-level permissions
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO replicator;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO replicator;
+
+-- Grant schema permissions for pglogical
+GRANT USAGE ON SCHEMA pglogical TO replicator;
+GRANT SELECT ON ALL TABLES IN SCHEMA pglogical TO replicator;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA pglogical TO replicator;
+
+-- Grant replication origin functions
+GRANT EXECUTE ON FUNCTION pg_replication_origin_session_setup(text) TO replicator;
+GRANT EXECUTE ON FUNCTION pg_replication_origin_session_reset() TO replicator;
+GRANT EXECUTE ON FUNCTION pg_replication_origin_create(text) TO replicator;
+GRANT EXECUTE ON FUNCTION pg_replication_origin_drop(text) TO replicator;
+GRANT EXECUTE ON FUNCTION pg_replication_origin_advance(text, pg_lsn) TO replicator;
+GRANT EXECUTE ON FUNCTION pg_replication_origin_progress(text, boolean) TO replicator;
+
+-- Grant parameter setting permissions
+GRANT SET ON PARAMETER session_replication_role TO replicator;
+ALTER USER replicator SET session_replication_role = 'replica';
+
+-- Grant access to system catalogs
+GRANT SELECT ON pg_replication_origin TO replicator;
+GRANT SELECT ON pg_replication_origin_status TO replicator;
+
+-- Make sure future tables are accessible
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO replicator;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO replicator;
+
+-- set conflict resolution
+ALTER SYSTEM SET pglogical.conflict_resolution = 'error';
+
+-- table
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(50)
+);
+
+-- create node1 on node1
+SELECT pglogical.create_node(
+    node_name := 'node1',
+    dsn := 'host={{NODE1_IP}} port=30432 dbname=my_db user=replicator password=test'
+);
+
+-- replicate table
+SELECT pglogical.replication_set_add_table('default', 'public.users');
+
+-- node1 -> node2
+SELECT pglogical.create_subscription(
+    subscription_name := 'sub_from_node2',
+    provider_dsn := 'host={{NODE2_IP}} port=30432 dbname=my_db user=replicator password=test',
+    forward_origins := '{}'
+);
+
+-- wait for sync (node1)
+SELECT pglogical.wait_for_subscription_sync_complete('sub_from_node2');
+
+-- verify
+SELECT * FROM pglogical.show_subscription_status();
+
+-- insert on node 1
+insert into users (name) values ('John Smith');
