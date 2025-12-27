0a8a7dc82c7b1c05eda18c39ca9e3478064692b8
Brandon Ros <brandonros1@gmail.com>
2025-12-26 19:29:22 -0500

    explain forward_origins

--- sql/templates/my_db-sync.sql
+++ sql/templates/my_db-sync.sql
@@ -1,4 +1,9 @@
--- __NODE_NAME__ -> __OTHER_NODE_NAME__
+-- Subscribe to changes from __OTHER_NODE_NAME__
+-- forward_origins := '{}' is critical for active-active setups. It prevents replication loops:
+--   - Without it, changes originating from THIS node would be sent to the other node,
+--     then replicated BACK here, then sent again... infinite loop.
+--   - '{}' means "don't forward changes that originated from other replication origins"
+--   - Only changes that originated locally on the provider are replicated.
 SELECT pglogical.create_subscription(
     subscription_name := 'sub_from___OTHER_NODE_NAME__',
     provider_dsn := 'host=__OTHER_NODE_IP__ port=30432 dbname=my_db user=replicator password=test',
